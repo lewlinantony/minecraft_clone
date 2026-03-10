d4f16942cf089013f7a21b72f17b441d36c21ac5
Lewlin Antony <lewlinantony@gmail.com>
2026-03-10 15:12:37 +0530

    updated the hash function with Magic primes for spatial hashing, which avoid a lot of hash collision -> more performance

--- src/core/utils.h
+++ src/core/utils.h
@@ -6,10 +6,11 @@ namespace std {
     template<>
     struct hash<glm::ivec3> {
         std::size_t operator()(const glm::ivec3& v) const noexcept {
-            std::size_t hx = std::hash<int>()(v.x);
-            std::size_t hy = std::hash<int>()(v.y);
-            std::size_t hz = std::hash<int>()(v.z);
-            return hx ^ (hy << 1) ^ (hz << 2);
+            // Magic primes for spatial hashing
+            std::size_t hx = std::hash<int>()(v.x) * 73856093;
+            std::size_t hy = std::hash<int>()(v.y) * 19349663;
+            std::size_t hz = std::hash<int>()(v.z) * 83492791;
+            return hx ^ hy ^ hz;
         }
     };
 }
